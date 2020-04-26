1fcc7aec2a69a7e77e850dacf33b08d45dd79c67
xtrp <36894883+xtrp@users.noreply.github.com>
2020-04-26 15:09:06 +0100

    Add basic image editing code for reference and testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rubiks_cube_visualizer
+# Rubik's Cube Visualizer
 Get a real-world looking image of a Rubik's cube with given piece colors.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+//! An example of generating julia fractals.
+extern crate image;
+extern crate num_complex;
+
+use image::Pixels;
+use image::Pixel;
+
 fn main() {
-    println!("Hello, world!");
+    use image::GenericImageView;
+    let image_path = "img/cube_edited_test.jpg";
+
+    let mut img = image::open(image_path).unwrap();
+
+    let img_width = img.dimensions().0;
+    let img_height = img.dimensions().1;
+
+    for p in img.pixels() {
+        println!("pixel: {:?}", p);
+    }
+
+    /*
+    // image dimension variables
+    let imgx = 800;
+    let imgy = 600;
+
+    // create new image buffer with dimensions of imgx and imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    // write blue image background
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = image::Rgb([0 as u8, 0, 255]);
+    }
+
+    // write lines
+    for x in 0..imgx {
+        for y in 0..imgy {
+            if ((x / 5 + 2) % 5 == 0) {
+                let pixel = imgbuf.get_pixel_mut(x, y);
+                *pixel = image::Rgb([0, 255, 0]);
+            }
+        }
+    }
+
+    // save image
+    imgbuf.save("fractal.png").unwrap();*/
 }
